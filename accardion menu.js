8d95b54eba1eaa7dbbbb2c018793db9a57580fd7
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-01-04 16:17:01 +0330

    js

---
+++ accardion menu.js
@@ -0,0 +1,6 @@
+const acc = document.querySelector(".accordion");
+const panel = document.querySelector(".panel");
+
+acc.addEventListener("click", function () {
+  acc.classList.toggle("active");
+});
