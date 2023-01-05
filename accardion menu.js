2791465c5f633bc9ecaa860cfef7fb7d9c3bb6e3
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-01-06 00:27:41 +0330

    side bar project

--- accardion menu.js
+++
@@ -1,6 +0,0 @@
-const acc = document.querySelector(".accordion");
-const panel = document.querySelector(".panel");
-
-acc.addEventListener("click", function () {
-  acc.classList.toggle("active");
-});
