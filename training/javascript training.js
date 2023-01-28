8338828d012153e1c45806d9c23d18c1477f609f
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-01-28 13:03:04 +0330

    moment.js format

--- training/javascript training.js
+++ training/javascript training.js
@@ -1,3 +1,10 @@
 let p = document.querySelector("p");
 
-let now = moment();
+// let now = moment().format('yyyy.MM.DD HH:mm:ss a');
+// p.innerHTML = now;
+
+
+setInterval(() => {
+  let now = moment().format('HH:MM:SS');
+  p.innerHTML = now;
+}, 1000);
