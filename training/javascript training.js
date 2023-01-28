b348af70820c98c77b116b5f892f862366ae9ada
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-01-28 17:02:00 +0330

    learning add() in moment.js

--- training/javascript training.js
+++ training/javascript training.js
@@ -1,10 +1,16 @@
-let p = document.querySelector("p");
+// let p = document.querySelector("p");
 
 // let now = moment().format('yyyy.MM.DD HH:mm:ss a');
 // p.innerHTML = now;
 
+// setInterval(() => {
+//   let now = moment().format("HH:MM:SS");
+//   p.innerHTML = now;
+// }, 1000);
 
-setInterval(() => {
-  let now = moment().format('HH:MM:SS');
-  p.innerHTML = now;
-}, 1000);
+// let date = moment("2022-05-6").isValid();
+// console.log(date);
+
+let now = moment();
+console.log(now.format("YYYY-MM-DD"));
+console.log(now.add(1, "month").format('YYYY-MM-DD'));
