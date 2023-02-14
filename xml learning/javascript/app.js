393fb11267bd79fc67fa7903a6296433bba9dd40
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-02-14 22:59:10 +0330

    ajax and json learning

---
+++ xml learning/javascript/app.js
@@ -0,0 +1,14 @@
+// const button = document.querySelector("button");
+// const output = document.querySelector("output");
+
+// button.addEventListener("click", ()=>{
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("GET", "../data.txt", true);
+//   xhr.onload = function (){
+//     if (this.status == 200){
+//       output.innerHTML = this.responseText;
+//       }
+      
+//   }
+//  xhr.send();
+// })
