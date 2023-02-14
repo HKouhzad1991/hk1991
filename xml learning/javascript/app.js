a9d761ebbc6d97b168d942ce1d9cd49e9db2902a
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-02-14 23:37:11 +0330

    json in ajax customer

--- xml learning/javascript/app.js
+++ xml learning/javascript/app.js
@@ -8,7 +8,32 @@
 //     if (this.status == 200){
 //       output.innerHTML = this.responseText;
 //       }
-      
+
 //   }
 //  xhr.send();
 // })
+const button = document.getElementById("button");
+
+button.addEventListener("click", loadCustomer);
+
+function loadCustomer() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("get", "../customer.json", true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let customer = JSON.parse(this.responseText);
+      let output = ` 
+        <ul>
+        <li>ID : ${customer.id}</li>
+        <li>firstName : ${customer.firstName}</li>
+        <li>lastName : ${customer.lastName}</li>
+        <li>phone : ${customer.phone}</li>
+        <li>age : ${customer.age}</li>
+        <li>gender : ${customer.gender}</li>
+        </ul>
+      `;
+      document.getElementById("customer").innerHTML = output;
+    }
+  };
+  xhr.send();
+}
