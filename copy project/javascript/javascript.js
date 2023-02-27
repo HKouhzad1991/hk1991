ff7ffd5674b855821b9a1db92c53ac3aa56dc72d
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-02-27 15:07:25 +0330

    copy to clipboard js

--- copy project/javascript/javascript.js
+++ copy project/javascript/javascript.js
@@ -1,1 +1,14 @@
-const
+const copyBtn = document.querySelector("button");
+const textarea = document.querySelector("textarea");
+
+copyBtn.addEventListener("click", () => {
+  textarea.select();
+  let texValue = textarea.value;
+
+  navigator.clipboard.writeText(texValue);
+  copyBtn.innerText = "کپی شد";
+  setTimeout(() => {
+    copyBtn.innerText = "کپی کن";
+    copyBtn.style.backgroundColor = "rgba(51, 49, 49, 0.421)";
+  }, 2000);
+});
